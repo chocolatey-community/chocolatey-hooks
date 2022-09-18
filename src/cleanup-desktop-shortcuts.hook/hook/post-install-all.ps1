da3076f99e883d5f2270d0fe03ee88b2a9c729da
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2022-09-18 17:46:59 -0500

    (#2) Add hook package to cleanup desktop shortcuts.

---
+++ src/cleanup-desktop-shortcuts.hook/hook/post-install-all.ps1
@@ -0,0 +1,33 @@
+﻿if (-not $packageScript) {
+    #Warning was provided in pre-install script
+    return
+}
+
+if ([string]::IsNullOrEmpty([Environment]::GetFolderPath("DesktopDirectory"))) {
+    Write-Warning "Skipping cleanup because user desktop directory cannot be found"
+    return
+}
+
+[array]$global:cleanup_desktop_shortcuts_hook_post_shortcuts = Get-Childitem -Path ([environment]::GetFolderPath("DesktopDirectory")) -Filter "*.lnk"
+
+if (Test-ProcessAdminRights) {
+    if ([string]::IsNullOrEmpty([Environment]::GetFolderPath("CommonDesktopDirectory"))) {
+        Write-Warning "System wide desktop directory cannot be found, something went wrong."
+        Write-Warning "Skipping cleanup of desktop shortcuts"
+        return
+    }
+    $global:cleanup_desktop_shortcuts_hook_post_shortcuts += Get-Childitem -Path ([environment]::GetFolderPath("CommonDesktopDirectory")) -Filter "*.lnk"
+} else {
+    Write-Host "Not cleaning desktop shortcuts from the system wide desktop directory as script is not running with Admin rights."
+}
+
+foreach ($postshortcut in $cleanup_desktop_shortcuts_hook_post_shortcuts) {
+    if (($null -ne $cleanup_desktop_shortcuts_hook_pre_shortcuts)) {
+        if ($cleanup_desktop_shortcuts_hook_pre_shortcuts.name.Contains($postshortcut.name)) {
+            Write-Debug "'$($postshortcut.name)' existed before install script run, ignoring"
+            continue;
+        }
+    }
+    Write-Host "Removing '$($postshortcut.name)'"
+    Remove-Item -Path $postshortcut.fullname
+}
---
+++ src/cleanup-desktop-shortcuts.hook/hook/pre-install-all.ps1
@@ -0,0 +1,19 @@
+﻿if (-not $packageScript) {
+    Write-Host "Skipping Cleanup of Desktop Shortcuts as there is no install script for this package"
+    return
+}
+
+if ([string]::IsNullOrEmpty([Environment]::GetFolderPath("DesktopDirectory"))) {
+    Write-Warning "User desktop directory cannot be found, is Chocolatey running as SYSTEM?"
+    return
+}
+
+[array]$global:cleanup_desktop_shortcuts_hook_pre_shortcuts = Get-Childitem -Path ([environment]::GetFolderPath("DesktopDirectory")) -Filter "*.lnk"
+
+if (Test-ProcessAdminRights) {
+    if ([string]::IsNullOrEmpty([Environment]::GetFolderPath("CommonDesktopDirectory"))) {
+        Write-Warning "System wide desktop directory cannot be found, something went wrong."
+        return
+    }
+    $global:cleanup_desktop_shortcuts_hook_pre_shortcuts += Get-Childitem -Path ([environment]::GetFolderPath("CommonDesktopDirectory")) -Filter "*.lnk"
+}
